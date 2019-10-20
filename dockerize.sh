15d4714f36a8964e6ee8041b446596ebedd54308
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-20 11:21:38 +0200

    Fixing tagging

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export VERSION=$(git tag --sort=-version:refname | head -1)
-docker build --no-cache -f ./Source/Dockerfile -t dolittle/runtime:$VERSION . --build-arg CONFIGURATION="Release"
+docker build --no-cache -f ./Dockerfile -t dolittle/runtime . --build-arg CONFIGURATION="Release"
+docker tag dolittle/runtime dolittle/runtime:$VERSION
+docker push dolittle/runtime
 docker push dolittle/runtime:$VERSION

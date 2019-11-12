2fdba840057c6edd03a2d6a9ff3598858365c1b2
Einar Ingebrigtsen <einar@dolittle.com>
2019-11-12 18:46:32 +0100

    Fixing so that it explicitly pushes latest + specific version

--- dockerize.sh
+++ dockerize.sh
@@ -2,5 +2,5 @@
 export VERSION=$(git tag --sort=-version:refname | head -1)
 docker build --no-cache -f ./Dockerfile -t dolittle/runtime . --build-arg CONFIGURATION="Release"
 docker tag dolittle/runtime dolittle/runtime:$VERSION
-docker push dolittle/runtime
+docker push dolittle/runtime:latest
 docker push dolittle/runtime:$VERSION
